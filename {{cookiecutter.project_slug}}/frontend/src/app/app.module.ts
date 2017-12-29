0e93ea9e25ac8cc46cf67c9bab1c6f03ea52aba6
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-12-29 04:37:52 -0400

    ✨ add routing default

--- {{cookiecutter.project_slug}}/frontend/src/app/app.module.ts
+++ {{cookiecutter.project_slug}}/frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
