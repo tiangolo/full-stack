8e4fca133856ecabe45394d8b2dc0901340010da
Sebastián Ramírez <tiangolo@gmail.com>
2018-01-08 10:26:58 +0300

    :white_check_mark: Make tests create first migration and do not remove created directory for debugging

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./testing-project
 
-cookiecutter --config-file ./testing-config.yml --no-input ./
+cookiecutter --config-file ./testing-config.yml --no-input -f ./
 
 cd ./testing-project
 
@@ -10,10 +10,8 @@ docker-compose -f docker-compose.test.yml build
 docker-compose -f docker-compose.test.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 docker-compose -f docker-compose.test.yml up -d
 sleep 20; # Give some time for the DB and prestart script to finish
-docker-compose -f docker-compose.test.yml run -T backend bash -c 'alembic revision --autogenerate -m "Testing" && alembic upgrade head'
+docker-compose -f docker-compose.test.yml exec -T backend bash -c 'alembic revision --autogenerate -m "Testing" && alembic upgrade head'
 docker-compose -f docker-compose.test.yml exec -T backend-rest-tests pytest
 docker-compose -f docker-compose.test.yml down -v --remove-orphans
 
 cd ../
-
-rm -rf ./testing-project
