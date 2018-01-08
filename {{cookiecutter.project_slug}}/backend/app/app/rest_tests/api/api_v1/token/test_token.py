ea9dc60a94d599b5f86e199f82f4ff0e27fb7290
Sebastián Ramírez <tiangolo@gmail.com>
2018-01-08 10:50:50 +0300

    :white_check_mark: Add tests for tokens

---
+++ {{cookiecutter.project_slug}}/backend/app/app/rest_tests/api/api_v1/token/test_token.py
@@ -0,0 +1,49 @@
+# Import installed packages
+import requests
+# Import app code
+from app.core import config
+
+
+def test_get_access_token(server_api):
+    login_data = {
+        'username': config.FIRST_SUPERUSER,
+        'password': config.FIRST_SUPERUSER_PASSWORD
+    }
+    r = requests.post(
+        f'{server_api}{config.API_V1_STR}/login/access-token', data=login_data)
+    tokens = r.json()
+    assert r.status_code == 200
+    assert 'access_token' in tokens
+    assert 'refresh_token' in tokens
+    assert tokens['access_token']
+    assert tokens['refresh_token']
+
+
+def test_use_access_token(server_api, superuser_token_headers):
+    r = requests.post(
+        f'{server_api}{config.API_V1_STR}/login/test-token',
+        headers=superuser_token_headers,
+        json={'test': 'test'})
+    result = r.json()
+    assert r.status_code == 200
+    assert 'id' in result
+
+
+def test_refresh_token(server_api):
+    login_data = {
+        'username': config.FIRST_SUPERUSER,
+        'password': config.FIRST_SUPERUSER_PASSWORD
+    }
+    r = requests.post(
+        f'{server_api}{config.API_V1_STR}/login/access-token', data=login_data)
+    tokens = r.json()
+    refresh_token = tokens['refresh_token']
+    headers = {
+        'Authorization': f'Bearer {refresh_token}'
+    }
+    r = requests.post(
+        f'{server_api}{config.API_V1_STR}/login/refresh-token',
+        headers=headers)
+    result = r.json()
+    assert r.status_code == 200
+    assert 'access_token' in result
