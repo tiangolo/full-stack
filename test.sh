0ce77cb34c85da59da71512563e7bffd4c346437
Sebastián Ramírez <tiangolo@gmail.com>
2018-02-27 18:49:49 +0400

    :white_check_mark: Update tests to use new Docker Compose files

--- test.sh
+++ test.sh
@@ -6,12 +6,13 @@ cookiecutter --config-file ./testing-config.yml --no-input -f ./
 
 cd ./testing-project
 
-docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
-docker-compose -f docker-compose.test.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.build.yml -f docker-compose.test.yml config > docker-stack.yml
+docker-compose -f docker-stack.yml build
+docker-compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
+docker-compose -f docker-stack.yml up -d
 sleep 20; # Give some time for the DB and prestart script to finish
-docker-compose -f docker-compose.test.yml exec -T backend bash -c 'alembic revision --autogenerate -m "Testing" && alembic upgrade head'
-docker-compose -f docker-compose.test.yml exec -T backend-rest-tests pytest
-docker-compose -f docker-compose.test.yml down -v --remove-orphans
+docker-compose -f docker-stack.yml exec -T backend bash -c 'alembic revision --autogenerate -m "Testing" && alembic upgrade head'
+docker-compose -f docker-stack.yml exec -T backend-rest-tests pytest
+docker-compose -f docker-stack.yml down -v --remove-orphans
 
 cd ../
