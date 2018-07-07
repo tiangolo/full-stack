bb9e352c897b67926dd658fa78a188e828f3e04c
Sebastián Ramírez <tiangolo@gmail.com>
2018-07-07 19:19:05 +0400

    :card_file_box: Update DB session generation

--- {{cookiecutter.project_slug}}/backend/app/app/db/external_session.py
+++ {{cookiecutter.project_slug}}/backend/app/app/db/external_session.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from app.core import config
 
 engine = create_engine(config.SQLALCHEMY_DATABASE_URI, convert_unicode=True)
-
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
+
+def get_session():
+    return db_session
