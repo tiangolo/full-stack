19322a7d9fdb8cb4e14728a81028f6c42ffdabcc
Sebastián Ramírez <tiangolo@gmail.com>
2018-09-24 19:18:10 +0400

    :white_check_mark: Update testing scripts
    make them re-usable shell scripts, that can be integrated into GitLab CI, or others like Travis or Drone

--- test.sh
+++ test.sh
@@ -6,11 +6,6 @@ cookiecutter --config-file ./testing-config.yml --no-input -f ./
 
 cd ./testing-project
 
-DOMAIN=backend docker-compose -f docker-compose.yml -f docker-compose.build.yml -f docker-compose.test.yml config > docker-stack.yml
-docker-compose -f docker-stack.yml build
-docker-compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
-docker-compose -f docker-stack.yml up -d
-docker-compose -f docker-stack.yml exec -T backend-tests /start.sh
-docker-compose -f docker-stack.yml down -v --remove-orphans
+bash ./script-test.sh
 
 cd ../
