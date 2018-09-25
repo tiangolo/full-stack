29ddc5009451aae3ce587a89336c4991faa2f54b
Sebastián Ramírez <tiangolo@gmail.com>
2018-09-26 02:06:20 +0400

    :white_check_mark: Upgrade and simplify tests utils, remove Faker
    as it is not actually needed

--- {{cookiecutter.project_slug}}/backend/app/app/tests/utils/faker.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from faker import Faker
-
-fake = Faker()
-
-use_seed = os.getenv("SEED")
-if use_seed:
-    fake.seed(use_seed)
--- {{cookiecutter.project_slug}}/backend/app/app/tests/utils/user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/utils/user.py
@@ -1,24 +1,6 @@
 import requests
 
-from .faker import fake
-
 from app.core import config
-
-
-def random_user(group_id=1):
-    first_name = fake.first_name()
-    last_name = fake.last_name()
-    email = fake.email()
-    app_id = f"{first_name}.{last_name}"
-    fake_user = {
-        "first_name": first_name,
-        "last_name": last_name,
-        "email": email,
-        "app_id": app_id,
-        "group_id": group_id,
-        "password": "passwordtest",
-    }
-    return fake_user
 
 
 def user_authentication_headers(server_api, email, password):
@@ -31,18 +13,3 @@ def user_authentication_headers(server_api, email, password):
     auth_token = response["access_token"]
     headers = {"Authorization": f"Bearer {auth_token}"}
     return headers
-
-
-def create_user(server_api, superuser_token_headers, user_data):
-    r = requests.post(
-        f"{server_api}{config.API_V1_STR}/users/",
-        headers=superuser_token_headers,
-        json=user_data,
-    )
-    created_user = r.json()
-    return created_user
-
-
-def create_random_user(server_api, superuser_token_headers):
-    user_data = random_user()
-    return create_user(server_api, superuser_token_headers, user_data)
