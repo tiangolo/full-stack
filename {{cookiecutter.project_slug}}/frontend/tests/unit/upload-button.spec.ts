9c3d26a3a616d2640e0899d1ef1700b42a92d4da
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-17 16:04:20 +0400

    :white_check_mark: Add simple test for UploadButton

--- {{cookiecutter.project_slug}}/frontend/tests/unit/upload-button.spec.ts
+++ {{cookiecutter.project_slug}}/frontend/tests/unit/upload-button.spec.ts
@@ -1,11 +1,14 @@
 import { shallowMount } from '@vue/test-utils';
 import UploadButton from '@/components/UploadButton.vue';
+import '@/plugins/vuetify';
 
 describe('UploadButton.vue', () => {
   it('renders props.title when passed', () => {
     const title = 'upload a file';
     const wrapper = shallowMount(UploadButton, {
-      propsData: { title },
+      slots: {
+        default: title,
+      },
     });
     expect(wrapper.text()).toMatch(title);
   });
