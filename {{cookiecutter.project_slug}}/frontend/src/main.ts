da42b84e33cbf897e87817ff75ad57e331ab0b10
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-17 16:05:29 +0400

    :art: Update structure of Vuetify import

--- {{cookiecutter.project_slug}}/frontend/src/main.ts
+++ {{cookiecutter.project_slug}}/frontend/src/main.ts
@@ -7,6 +7,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false;
 
