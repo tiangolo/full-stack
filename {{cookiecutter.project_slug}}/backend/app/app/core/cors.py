43f28aa36ccfe26e26e128c2384442024266697f
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-17 11:37:38 +0400

    :recycle: Read CORS as env vars instead of fixed RegEx

--- {{cookiecutter.project_slug}}/backend/app/app/core/cors.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/cors.py
@@ -8,9 +8,15 @@ from flask_cors import CORS
 from app.main import app
 from app.core import config
 
-# Anything from SERVER_NAME
-use_domain = config.SERVER_NAME.replace(".", r"\.")
-cors_origins_regex = re.compile(
-    r"^(https?:\/\/(?:.+\.)?(" + use_domain + r")(?::\d{1,5})?)$"
-)
-CORS(app, origins=cors_origins_regex, supports_credentials=True)
+origins = []
+
+print(config.BACKEND_CORS_ORIGINS)
+
+# Set all CORS enabled origins
+if config.BACKEND_CORS_ORIGINS:
+    origins_raw = config.BACKEND_CORS_ORIGINS.split(",")
+    for origin in origins_raw:
+        use_origin = origin.strip()
+        origins.append(use_origin)
+
+    CORS(app, origins=origins, supports_credentials=True)
