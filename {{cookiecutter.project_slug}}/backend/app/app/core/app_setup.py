4029345cf16f5d9ded8a7936ec591291b12dece4
Sebastián Ramírez <tiangolo@gmail.com>
2018-10-20 17:25:56 +0400

    :sparkles: Don't limit Flask to a single domain
    as it is not needed nor being used

--- {{cookiecutter.project_slug}}/backend/app/app/core/app_setup.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/app_setup.py
@@ -18,7 +18,6 @@ from . import errors  # noqa
 from ..api.api_v1 import api as api_v1  # noqa
 
 app.config["SECRET_KEY"] = config.SECRET_KEY
-app.config["SERVER_NAME"] = config.SERVER_NAME
 
 sentry = Sentry(app, dsn=config.SENTRY_DSN)
 
