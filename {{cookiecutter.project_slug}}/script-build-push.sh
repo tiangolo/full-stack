b46318d75868378ffbf60b499d74842be9b90c9e
Sebastián Ramírez <tiangolo@gmail.com>
2018-11-03 09:53:08 +0400

    :recycle: Re-structure scripts to allow only build

--- {{cookiecutter.project_slug}}/script-build-push.sh
+++ {{cookiecutter.project_slug}}/script-build-push.sh
@@ -5,11 +5,6 @@ set -e
 
 TAG=${TAG} \
 FRONTEND_ENV=${FRONTEND_ENV-production} \
-docker-compose \
--f docker-compose.deploy.build.yml \
--f docker-compose.deploy.images.yml \
-config > docker-stack.yml
-
-docker-compose -f docker-stack.yml build
+source ./script-build.sh
 
 docker-compose -f docker-stack.yml push
